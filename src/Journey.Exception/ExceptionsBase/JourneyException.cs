626c7a3c599ec6edbd3089d4635b5f8689a50d5d
Carlos Furtado <fonizetefurado24@gmail.com>
2024-07-09 22:48:06 -0300

    Add project files.

---
+++ src/Journey.Exception/ExceptionsBase/JourneyException.cs
@@ -0,0 +1,8 @@
+﻿namespace Journey.Exception.ExceptionsBase;
+public class JourneyException : SystemException
+{
+    public JourneyException(string massage) : base(massage)
+    {
+        
+    }
+}
